a8c548afd00feafef944977238b53906d3876a92
rijoulgemi-manhonnor <rijoulgemi@gmail.com>
2026-03-18 12:01:51 +0100

    Update communes.py

--- communes.py
+++ communes.py
@@ -1,26 +1,233 @@
 COMMUNES = [
-    {"name": "Meknès", "province": "Préfecture de Meknès", "type": "Urbaine", "search_name": "Meknès, Morocco"},
-    {"name": "Ouislane", "province": "Préfecture de Meknès", "type": "Urbaine", "search_name": "Ouislane, Meknès, Morocco"},
-    {"name": "Toulal", "province": "Préfecture de Meknès", "type": "Urbaine", "search_name": "Toulal, Meknès, Morocco"},
-    {"name": "Boufekrane", "province": "Préfecture de Meknès", "type": "Urbaine", "search_name": "Boufekrane, Morocco"},
-    {"name": "Fès", "province": "Préfecture de Fès", "type": "Urbaine", "search_name": "Fès, Morocco"},
-    {"name": "El Hajeb", "province": "El Hajeb", "type": "Urbaine", "search_name": "El Hajeb, Morocco"},
-    {"name": "Ifrane", "province": "Ifrane", "type": "Urbaine", "search_name": "Ifrane, Morocco"},
-    {"name": "Azrou", "province": "Ifrane", "type": "Urbaine", "search_name": "Azrou, Morocco"},
-    {"name": "Sefrou", "province": "Sefrou", "type": "Urbaine", "search_name": "Sefrou, Morocco"},
-    {"name": "Missour", "province": "Boulemane", "type": "Urbaine", "search_name": "Missour, Morocco"},
-    {"name": "Taounate", "province": "Taounate", "type": "Urbaine", "search_name": "Taounate, Morocco"},
-    {"name": "Taza", "province": "Taza", "type": "Urbaine", "search_name": "Taza, Morocco"},
-    {"name": "Guercif", "province": "Guercif", "type": "Urbaine", "search_name": "Guercif, Morocco"}
-]
+    # ================================================================
+    # PRÉFECTURE DE MEKNÈS
+    # ================================================================
+    {
+        "name": "Meknès",
+        "province": "Préfecture de Meknès",
+        "type": "Urbaine",
+        "search_name": "Meknès, Morocco"
+    },
+    {
+        "name": "Ouislane",
+        "province": "Préfecture de Meknès",
+        "type": "Urbaine",
+        "search_name": "Ouislane, Meknès, Morocco"
+    },
+    {
+        "name": "Toulal",
+        "province": "Préfecture de Meknès",
+        "type": "Urbaine",
+        "search_name": "Toulal, Meknès, Morocco"
+    },
+    {
+        "name": "Boufekrane",
+        "province": "Préfecture de Meknès",
+        "type": "Urbaine",
+        "search_name": "Boufekrane, Morocco"
+    },
 
-COMMUNES_RURALES = [
-    {"name": "Dkhissa", "province": "Préfecture de Meknès", "type": "Rurale", "search_name": "Dkhissa, Meknès, Morocco"},
-    {"name": "Ain Leuh", "province": "Ifrane", "type": "Rurale", "search_name": "Ain Leuh, Morocco"},
-    {"name": "Guigou", "province": "Boulemane", "type": "Rurale", "search_name": "Guigou, Morocco"}
-]
+    # ================================================================
+    # PRÉFECTURE DE FÈS
+    # ================================================================
+    {
+        "name": "Fès",
+        "province": "Préfecture de Fès",
+        "type": "Urbaine",
+        "search_name": "Fès, Morocco"
+    },
+    {
+        "name": "Mechouar Fès Jdid",
+        "province": "Préfecture de Fès",
+        "type": "Urbaine",
+        "search_name": "Fes Jdid, Fès, Morocco"
+    },
 
-def get_all_communes(include_rural=True):
-    if include_rural:
-        return COMMUNES + COMMUNES_RURALES
-    return COMMUNES
+    # ================================================================
+    # PROVINCE D'EL HAJEB
+    # ================================================================
+    {
+        "name": "El Hajeb",
+        "province": "El Hajeb",
+        "type": "Urbaine",
+        "search_name": "El Hajeb, Morocco"
+    },
+    {
+        "name": "Sabaa Aiyoun",
+        "province": "El Hajeb",
+        "type": "Urbaine",
+        "search_name": "Sabaa Aiyoun, El Hajeb, Morocco"
+    },
+    {
+        "name": "Agourai",
+        "province": "El Hajeb",
+        "type": "Urbaine",
+        "search_name": "Agourai, Morocco"
+    },
+    {
+        "name": "Ain Taoujdate",
+        "province": "El Hajeb",
+        "type": "Urbaine",
+        "search_name": "Ain Taoujdate, Morocco"
+    },
+
+    # ================================================================
+    # PROVINCE D'IFRANE
+    # ================================================================
+    {
+        "name": "Ifrane",
+        "province": "Ifrane",
+        "type": "Urbaine",
+        "search_name": "Ifrane, Morocco"
+    },
+    {
+        "name": "Azrou",
+        "province": "Ifrane",
+        "type": "Urbaine",
+        "search_name": "Azrou, Morocco"
+    },
+
+    # ================================================================
+    # PROVINCE DE MOULAY YACOUB
+    # ================================================================
+    {
+        "name": "Moulay Yacoub",
+        "province": "Moulay Yacoub",
+        "type": "Urbaine",
+        "search_name": "Moulay Yacoub, Morocco"
+    },
+
+    # ================================================================
+    # PROVINCE DE SEFROU
+    # ================================================================
+    {
+        "name": "Sefrou",
+        "province": "Sefrou",
+        "type": "Urbaine",
+        "search_name": "Sefrou, Morocco"
+    },
+    {
+        "name": "Bhalil",
+        "province": "Sefrou",
+        "type": "Urbaine",
+        "search_name": "Bhalil, Sefrou, Morocco"
+    },
+    {
+        "name": "El Menzel",
+        "province": "Sefrou",
+        "type": "Urbaine",
+        "search_name": "El Menzel, Sefrou, Morocco"
+    },
+    {
+        "name": "Ribate El Kheir",
+        "province": "Sefrou",
+        "type": "Urbaine",
+        "search_name": "Ribate El Kheir, Morocco"
+    },
+    {
+        "name": "Imouzzer Kandar",
+        "province": "Sefrou",
+        "type": "Urbaine",
+        "search_name": "Imouzzer Kandar, Morocco"
+    },
+
+    # ================================================================
+    # PROVINCE DE BOULEMANE
+    # ================================================================
+    {
+        "name": "Boulemane",
+        "province": "Boulemane",
+        "type": "Urbaine",
+        "search_name": "Boulemane, Morocco"
+    },
+    {
+        "name": "Missour",
+        "province": "Boulemane",
+        "type": "Urbaine",
+        "search_name": "Missour, Morocco"
+    },
+    {
+        "name": "Outat El Haj",
+        "province": "Boulemane",
+        "type": "Urbaine",
+        "search_name": "Outat El Haj, Morocco"
+    },
+
+    # ================================================================
+    # PROVINCE DE TAOUNATE
+    # ================================================================
+    {
+        "name": "Taounate",
+        "province": "Taounate",
+        "type": "Urbaine",
+        "search_name": "Taounate, Morocco"
+    },
+    {
+        "name": "Tissa",
+        "province": "Taounate",
+        "type": "Urbaine",
+        "search_name": "Tissa, Taounate, Morocco"
+    },
+    {
+        "name": "Ghafsai",
+        "province": "Taounate",
+        "type": "Urbaine",
+        "search_name": "Ghafsai, Morocco"
+    },
+    {
+        "name": "Kariat Ba Mohamed",
+        "province": "Taounate",
+        "type": "Urbaine",
+        "search_name": "Kariat Ba Mohamed, Morocco"
+    },
+    {
+        "name": "Ain Aicha",
+        "province": "Taounate",
+        "type": "Urbaine",
+        "search_name": "Ain Aicha, Taounate, Morocco"
+    },
+    {
+        "name": "Thar Es Souk",
+        "province": "Taounate",
+        "type": "Urbaine",
+        "search_name": "Thar Es Souk, Morocco"
+    },
+
+    # ================================================================
+    # PROVINCE DE TAZA
+    # ================================================================
+    {
+        "name": "Taza",
+        "province": "Taza",
+        "type": "Urbaine",
+        "search_name": "Taza, Morocco"
+    },
+    {
+        "name": "Oued Amlil",
+        "province": "Taza",
+        "type": "Urbaine",
+        "search_name": "Oued Amlil, Taza, Morocco"
+    },
+    {
+        "name": "Tahla",
+        "province": "Taza",
+        "type": "Urbaine",
+        "search_name": "Tahla, Taza, Morocco"
+    },
+    {
+        "name": "Aknoul",
+        "province": "Taza",
+        "type": "Urbaine",
+        "search_name": "Aknoul, Morocco"
+    },
+
+    # ================================================================
+    # PROVINCE DE GUERCIF
+    # ================================================================
+    {
+        "name": "Guercif",
+        "province": "Guercif",
+        "type": "Urbaine",
+        "search_name": "Guercif, Morocco"
+    },
+]
